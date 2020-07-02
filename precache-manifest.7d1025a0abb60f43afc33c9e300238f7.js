1f96eb3568128d0707783b675b085e6f194a4154
Jack Sun <syy1125@hotmail.com>
2020-07-02 13:49:35 -0700

    v0.1.7

---
+++ precache-manifest.7d1025a0abb60f43afc33c9e300238f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ad24b211f8f968f3a5fb0cab428770",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68e30667a75fa3f47c8a",
+    "url": "/static/css/main.1eafc267.chunk.css"
+  },
+  {
+    "revision": "72149049b1ce768ea31f",
+    "url": "/static/js/2.d7c2f2dc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.d7c2f2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e30667a75fa3f47c8a",
+    "url": "/static/js/main.c8831b45.chunk.js"
+  },
+  {
+    "revision": "ec46c55ef156c21bfa51",
+    "url": "/static/js/runtime-main.a0d7f118.js"
+  }
+]);
---
+++ resources/about.md
@@ -0,0 +1,11 @@
+## About This Website
+
+Yes, I know there are a lot of site creation tools out there, designed to make appealing portfolio websites.
+
+So let me make one thing absolutely clear: I'm a programmer, not a graphics designer. I'm much more at home dealing with code than with WYSIWYG editors.
+
+Therefore, this website is written in React. This web page is written in markdown, and rendered in the page using an open-source markdown renderer `react-markdown`.
+
+### Technology stack
+
+- [typescript]()
